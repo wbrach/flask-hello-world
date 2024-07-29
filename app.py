2e3e40aa204cf9155631bb5dd4d5c32d2bbc3d88
Weston Brach <wbrach48@gmail.com>
2024-07-29 17:25:10 +0000

    db_insert route
    db_insert route

--- app.py
+++ app.py
@@ -29,4 +29,21 @@ def creating():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
+
+
+@app.route('/db_insert')
+def db_insert():
+    conn = psycopg2.connect("postgresql://database_example_for_lab_user:qvnuRwvx7q2cvBRV1xNY4MECSJnasLsN@dpg-cqjsad6ehbks73chrcl0-a/database_example_for_lab")
+    cur = conn.cursor()
+    cur.execute('''
+    INSERT INTO Basketball (First, Last, City, Name, Number)
+            VALUES
+            ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+            ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+            ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+            ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
     
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
