b366e23eddb4cba3580738603c8f1db53e45adeb
Weston Brach <wbrach48@gmail.com>
2024-07-29 17:11:18 +0000

    db_create route successfully made
    db_create route successfully made

--- app.py
+++ app.py
@@ -12,3 +12,21 @@ def testing():
     conn.close()
     return "Database connection is successful"
 
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgresql://database_example_for_lab_user:qvnuRwvx7q2cvBRV1xNY4MECSJnasLsN@dpg-cqjsad6ehbks73chrcl0-a/database_example_for_lab")
+    cur = conn.cursor()
+    cur.execute('''
+            CREATE TABLE IF NOT EXISTS Basketball(
+                First varchar(255),
+                Last varchar(255),
+                City varchar(255),
+                Name varchar(255),
+                Number int
+            );
+        ''')
+    
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
+    
